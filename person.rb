9c22d110925893af45f40fda25987fd969f35891
tugsem <saranlitugsem@gmail.com>
2022-07-27 15:00:57 +0300

    add associations

--- person.rb
+++ person.rb
@@ -6,10 +6,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def can_use_services?
