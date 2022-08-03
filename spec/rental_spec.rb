a064cd0a25db36715fd2f4ba8d84371959d434d8
unknown <ibr.s.ahmat@gmail.com>
2022-08-03 21:19:42 +0300

    Fix linters errors

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Rental do
   it 'create new rental with date, book and name' do
-    @person = Teacher.new(58, "Max", "CS")
-    @book = Book.new('Harry Potter', "Joanne Rolking")
-    @rental = Rental.new("2022/08/03", @book, @person)
-    
+    @person = Teacher.new(58, 'Max', 'CS')
+    @book = Book.new('Harry Potter', 'Joanne Rolking')
+    @rental = Rental.new('2022/08/03', @book, @person)
+
     expect(@rental.book.title).to eql(@book.title)
     expect(@rental.person.name).to eql(@person.name)
     expect(@rental).to be_an_instance_of Rental
