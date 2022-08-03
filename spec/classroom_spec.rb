a6a6ff5e87aab800aedd582e72d0cba4f93aa83c
unknown <ibr.s.ahmat@gmail.com>
2022-08-03 21:17:59 +0300

    Edit the describtion

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Classroom do
-  it 'create new person with age and name' do
+  it 'create new classroom' do
     @classroom = Classroom.new('C.S.')
 
     expect(@classroom).to have_attributes(label: 'C.S.')
