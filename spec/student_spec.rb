266d06595615a3fc599010462aac286be82a7c73
tugsem <saranlitugsem@gmail.com>
2022-08-03 18:42:02 +0300

    Add test for student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Student do
+  it 'create new student with age and name' do
+    @student = Student.new(12, 'Frank', false, '9C')
+
+    expect(@student).to have_attributes(age: 12)
+    expect(@student).to have_attributes(name: 'Frank')
+    expect(@student).to have_attributes(classroom: '9C')
+    expect(@student).to be_an_instance_of Student
+  end
+end
